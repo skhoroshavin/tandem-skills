db57f0358f35735c39d9943dc587b4661bbb8adc
Sergey Khoroshavin <skhoroshavin@gmail.com>
2026-09-02 20:58:12 +0200

    Remove web_search from pi package

--- packages/pi-tandem/extensions/tandem/index.ts
+++ packages/pi-tandem/extensions/tandem/index.ts
@@ -1,8 +1,6 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import injectProjectContext from "./pi-tandem";
-import registerWebSearch from "./web-search";
 
 export default function (pi: ExtensionAPI) {
   injectProjectContext(pi);
-  registerWebSearch(pi);
 }
